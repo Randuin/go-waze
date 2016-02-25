3e4c5f704929cf6b1602f6fd4e9583b23c5d6d83
Robin Liao <randuin@gmail.com>
2016-02-24 22:24:57 -0800

    Use Lat & Lng instead of X & Y

--- waze.go
+++ waze.go
@@ -1,75 +1,74 @@
 package waze
 
 import (
-  "fmt"
-  "net/url"
-  "net/http"
-  "strings"
-  "encoding/json"
-  "io/ioutil"
-  "bytes"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
 )
 
 type Request struct {
-  From *Coord
-  To *Coord
+	From *Coord
+	To   *Coord
 }
 
 func (request Request) GetResponse() *Response {
-  resp, _ := http.Get(request.BuildURL())
-  response := ResponseConstructor(resp)
-  return response
+	resp, _ := http.Get(request.BuildURL())
+	response := ResponseConstructor(resp)
+	return response
 }
 
 func (request Request) BuildURL() string {
-  str := fmt.Sprintf("https://www.waze.com/RoutingManager/routingRequest?from=%s&to=%s&at=0&returnJSON=true&returnGeometries=true&returnInstructions=true&timeout=60000&nPaths=3",
-    request.From.MakeString(),
-    request.To.MakeString())
+	str := fmt.Sprintf("https://www.waze.com/RoutingManager/routingRequest?from=%s&to=%s&at=0&returnJSON=true&returnGeometries=true&returnInstructions=true&timeout=60000&nPaths=3",
+		request.From.MakeString(),
+		request.To.MakeString())
 
-  return str
+	return str
 }
 
-
 type Response struct {
-  Alternatives []Alternative `json:"alternatives"`
+	Alternatives []Alternative `json:"alternatives"`
 }
 
 func ResponseConstructor(response *http.Response) *Response {
-  str, _ := ioutil.ReadAll(response.Body)
-  str = bytes.Replace(str, []byte("NaN"), []byte("null"), -1)
-  resp := &Response{}
+	str, _ := ioutil.ReadAll(response.Body)
+	str = bytes.Replace(str, []byte("NaN"), []byte("null"), -1)
+	resp := &Response{}
 
-  json.Unmarshal(str, &resp)
-  return resp
+	json.Unmarshal(str, &resp)
+	return resp
 }
 
 type Alternative struct {
-  Response struct {
-    RouteName string `json:"routeName"`
-    Results []Result `json:"results"`
-  }
+	Response struct {
+		RouteName string   `json:"routeName"`
+		Results   []Result `json:"results"`
+	}
 }
 
 func (a Alternative) TotalCrossTime() int {
-  sum := 0
-  for _, result := range a.Response.Results {
-    sum += result.CrossTime
-  }
-  return sum
+	sum := 0
+	for _, result := range a.Response.Results {
+		sum += result.CrossTime
+	}
+	return sum
 }
 
 type Result struct {
-  CrossTime int `json:"crossTime"`
+	CrossTime int `json:"crossTime"`
 }
 
 type Coord struct {
-  X float64
-  Y float64
+	Lat float64
+	Lng float64
 }
 
 func (c Coord) MakeString() string {
-  str := fmt.Sprintf("x:%f+y:%f", c.X, c.Y)
-  str = url.QueryEscape(str)
-  str = strings.Replace(str, "%2B", "+", 1)
-  return str
+	str := fmt.Sprintf("x:%f+y:%f", c.Lat, c.Lng)
+	str = url.QueryEscape(str)
+	str = strings.Replace(str, "%2B", "+", 1)
+	return str
 }
